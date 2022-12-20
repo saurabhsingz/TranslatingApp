6238b83b9ba929e923424b6b0c19dc9efdf05493
saurabhsingz <68174693+saurabhsingz@users.noreply.github.com>
2022-12-20 21:23:40 +0530

    Format done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-
-import './App.css';
+import "./App.css";
 import { useEffect, useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 function App() {
-
   const [txt, settxt] = useState("");
   const [resulttxt, resultsettxt] = useState("");
   const [languagelist, setlanglist] = useState([]);
@@ -12,47 +10,45 @@ function App() {
   const [declangkey, decsetlangkey] = useState("");
 
   useEffect(() => {
-    axios.get('https://libretranslate.com/languages')
-      .then(response => {
-        setlanglist(response.data)
-      })
-      getlanguagesource();
-  }, [txt])
+    axios.get("https://libretranslate.com/languages").then((response) => {
+      setlanglist(response.data);
+    });
+    getlanguagesource();
+  }, [txt]);
 
   function setinputarea(e) {
-    
-    settxt(e.target.value)
-    
+    settxt(e.target.value);
   }
 
-  function getlanguagesource(){
-    let data1={
-      q:txt
-    }
-    axios.post('https://libretranslate.de/detect',data1)
-    .then(response=>{
-      decsetlangkey(response.data[0].language)
-    })
+  function getlanguagesource() {
+    let data1 = {
+      q: txt,
+    };
+    axios.post("https://libretranslate.de/detect", data1).then((response) => {
+      decsetlangkey(response.data[0].language);
+    });
   }
 
   function translatetext() {
-    let data={
-      q:txt,
+    let data = {
+      q: txt,
       source: declangkey,
-      target: langkey
+      target: langkey,
+    };
+    if (data.q == "") {
+      resultsettxt("*Please Type Something");
+    } else if (data.target == "")
+      resultsettxt("*Please Select Desired Language");
+    else {
+      axios
+        .post("https://libretranslate.de/translate", data)
+        .then((response) => {
+          resultsettxt(response.data.translatedText);
+        });
     }
-    if(data.q==""){
-      resultsettxt("*Please Type Something")
-    }
-    else if(data.target=="")resultsettxt("*Please Select Desired Language")
-    else
-    {axios.post('https://libretranslate.de/translate',data)
-    .then(response=>{
-      resultsettxt(response.data.translatedText)
-    })}
   }
   function languagekey(e) {
-    setlangkey(e.target.value)
+    setlangkey(e.target.value);
   }
   return (
     <div className="container">
@@ -60,20 +56,37 @@ function App() {
         <div className="col-md-12">
           <h1 className="mt-3 position-relative font">⌨️ Translate</h1>
           <div className="form-group shadow-textarea mt-4">
-            <textarea onChange={setinputarea} className="form-control z-depth-1" id="exampleFormControlTextarea6" rows="5" placeholder="Enter the Text you want to Translate..."></textarea>
+            <textarea
+              onChange={setinputarea}
+              className="form-control z-depth-1"
+              id="exampleFormControlTextarea6"
+              rows="5"
+              placeholder="Enter the Text you want to Translate..."
+            ></textarea>
           </div>
           <div className="form-group" aria-label="Default select example">
             <select className="form-control" id="sel1" onChange={languagekey}>
-              <option value="none" selected>Select Translating Language</option>
+              <option value="none" selected>
+                Select Translating Language
+              </option>
               {languagelist.map((language) => {
-                return <option value={language.code}>{language.name}</option>
+                return <option value={language.code}>{language.name}</option>;
               })}
             </select>
           </div>
           <div className="form-group shadow-textarea mt-4">
-            <textarea disabled className="form-control z-depth-1" id="exampleFormControlTextarea6" rows="5" placeholder="Translated Text will show Here..." value={resulttxt}></textarea>
+            <textarea
+              disabled
+              className="form-control z-depth-1"
+              id="exampleFormControlTextarea6"
+              rows="5"
+              placeholder="Translated Text will show Here..."
+              value={resulttxt}
+            ></textarea>
           </div>
-          <button onClick={translatetext} className="btn btn-primary">Translate</button>
+          <button onClick={translatetext} className="btn btn-primary">
+            Translate
+          </button>
         </div>
       </div>
     </div>
